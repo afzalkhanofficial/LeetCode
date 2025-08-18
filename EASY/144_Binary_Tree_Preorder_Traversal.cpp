ff0455e32182485b574060f9d5ec752086e6b3a9
Afzal <149293919+afzalkhanofficial@users.noreply.github.com>
2025-08-18 23:33:05 +0530

    Create 144_Binary_Tree_Preorder_Traversal.cpp

---
+++ EASY/144_Binary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        if (root == nullptr) return result;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while (!st.empty()) {
+            TreeNode* current = st.top();
+            st.pop();
+            result.push_back(current->val);
+
+            if (current->right) st.push(current->right);
+            if (current->left) st.push(current->left);
+        }
+
+        return result;
+    }
+};
+
+// Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+// Example 1:
+// Input: root = [1,null,2,3]
+// Output: [1,2,3]
+// Example 2:
+// Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+// Output: [1,2,4,5,6,7,3,8,9]
+// Example 3:
+// Input: root = []
+// Output: []
+// Example 4:
+// Input: root = [1]
+// Output: [1]
+
+// Constraints:
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
