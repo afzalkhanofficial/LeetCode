46cbc44d038835cbf20678f9a8754901188eee05
Afzal <149293919+afzalkhanofficial@users.noreply.github.com>
2025-08-25 22:41:08 +0530

    Create 190_Reverse_Bits.cpp

---
+++ EASY/190_Reverse_Bits.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int reverseBits(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; ++i)
+            if (n >> i & 1)
+                ans |= 1 << (31 - i);
+        return ans;
+    }
+};
+
+// Reverse bits of a given 32 bits signed integer.
+
+// Example 1:
+// Input: n = 43261596
+// Output: 964176192
+// Explanation:
+// Integer	Binary
+// 43261596	00000010100101000001111010011100
+// 964176192	00111001011110000010100101000000
+// Example 2:
+// Input: n = 2147483644
+// Output: 1073741822
+// Explanation:
+// Integer	Binary
+// 2147483644	01111111111111111111111111111100
+// 1073741822	00111111111111111111111111111110
+ 
+// Constraints:
+// 0 <= n <= 231 - 2
+// n is even.
